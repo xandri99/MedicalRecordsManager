1947ce11defb043b98035b46257b11a864edaf60
xandri99 <65783989+xandri99@users.noreply.github.com>
2021-03-11 17:33:15 +0100

    Add files via upload

--- Menudef.py
+++ Menudef.py
@@ -1,3 +1,7 @@
+import sys
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, 
+                             QToolTip, QMessageBox, QLabel)
 from PyQt5.QtWidgets import QPushButton, QLineEdit, QApplication, QFormLayout, QWidget, QTextEdit, QSpinBox, QListWidget
 from PyQt5.QtWidgets import QApplication, QWidget, QComboBox, QPushButton
 
@@ -40,18 +44,41 @@ class FormWindow(QWidget):
     def getComboValue(self):
         print((self.comboBox.currentText(), self.comboBox.currentIndex()))
 
-app = QApplication([])
-w = FormWindow()
-w.show()
-app.exec()
 
 
 
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "Menu"
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("Medical Form", self)
+        self.pushButton.move(275, 200)
+
+        self.pushButton.clicked.connect(self.medicalForm)              # <===
+        self.main_window()
 
 
 
-#https://www.learnpyqt.com/examples/python-pdf-report-generator/
+    def main_window(self):
+        self.label = QLabel("Medical Forms Administrator", self)
+        self.label.move(285, 175)
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
 
-#https://realpython.com/python-pyqt-layout/
 
-#https://pythones.net/lista-de-widgets-pyqt-5-explicacion-de-cada-uno/
+    def medicalForm(self):                                             # <===
+        self.w = FormWindow()
+        self.w.show()
+        self.hide()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec())
